050796dc8721eaa622e980d5b56264a69180a591
py85329140 <3293406747@qq.com>
2022-09-28 15:17:33 +0800

    优化代码结构

--- common/request.py
+++ common/request.py
@@ -16,72 +16,14 @@ path = Path(__file__).resolve()
 session = requests.session()
 extractPool = {}
 
-
-def autoSendRequest(caseinfo, start=None):
-	""" 获取用例自动发送请求 """
-	temp = dict(
-		url=caseinfo["request"].pop("url"),
-		method=caseinfo["request"].pop("method"),
-		base_url=caseinfo["base_url"],
-		files= caseinfo["request"].pop("files") if caseinfo["request"].get("files") else None
-	)
-	response = send_request(caseinfo=caseinfo, start=start,**temp,**caseinfo["request"])
-	return response
-
-def render_template(data):
-	""" 渲染用例 """
-	data = json.dumps(data) if data and isinstance(data, dict) else data
-	extract = extractPool
-	# extract.yaml文件可能为空，为空时不渲染用例
-	if extract and data:
-		temp = Template(data).render(**extract)
-		response = yaml.load(stream=temp, Loader=yaml.FullLoader)
-		return response
-	else:
-		return None
-
-
-def send_request(method, url, base_url=None, files=None, caseinfo=None, start=None, **kwargs):
-	""" 发送同一个session请求 """
-	method = str(method).lower()
-	new_url = render_template(url)
-	url = new_url if new_url else url		# 未经过渲染时使用原来的url，未渲染会导致url变为None
-	url = base_url + url if base_url else url  # 配置中无base_url或者send_request方法不传入base_url参数，都会使base_url为None
-	for key, value in kwargs.items():
-		if key in ("headers", "params", "data", "json"):
-			new_value = render_template(value)
-			kwargs[key] = new_value if new_value else value
-	logger.info(f"{'接口请求开始':-^20}")
-	if caseinfo:
+def caseHandle(func):
+	""" 用例处理 """
+	def wapper(caseinfo, start=None):
+		logger.info(f"{'接口请求开始':-^20}")
 		logger.info(f"请求名称:{caseinfo['name']}")
-	logger.info(f"请求url:{url}")
-	logger.info(f"请求方法:{method}")
-	logger.info(f"请求参数:{kwargs}")
-	logger.info(f"文件上传:{files}")
-	allure.attach(body=url, name="请求url:", attachment_type=allure.attachment_type.TEXT)
-	allure.attach(body=method, name="请求方式:", attachment_type=allure.attachment_type.TEXT)
-	allure.attach(body=json.dumps(kwargs, ensure_ascii=False), name="请求参数:",
-				  attachment_type=allure.attachment_type.TEXT)
-	if files:
-		allure.attach(body=json.dumps(files, ensure_ascii=False), name="文件上传:",
-					  attachment_type=allure.attachment_type.TEXT)
-		for k, v in files.items():
-			files[k] = open(v, "rb")
-	response = session.request(method=method, url=url, files=files,timeout=10,**kwargs)
-	allure.attach(body=str(response.status_code), name="响应状态码:", attachment_type=allure.attachment_type.TEXT)
-	try:
-		data = json.dumps(response.json(), ensure_ascii=False)
-	except Exception:
-		response.encoding = "utf-8"
-		data = response.text
-	finally:
-		allure.attach(body=data, name="响应数据:", attachment_type=allure.attachment_type.TEXT)
-	type_ = ["image/jpeg", "image/png", "application/pdf"]
-	download(response, type_)
-	if caseinfo:
+		response = func(caseinfo)
 		extract_variable(response, caseinfo, start=start)
-		new_validata = render_template(caseinfo["validata"])
-		caseinfo["validata"] = new_validata if new_validata else caseinfo["validata"]
+		caseinfo["validata"] = render_template(caseinfo["validata"])
 		if caseinfo["validata"]:
 			p = r'select\s*(.*?)\sfrom'
 			if caseinfo["validata"].get('equal'):
@@ -100,6 +42,95 @@ def send_request(method, url, base_url=None, files=None, caseinfo=None, start=No
 						num += 1
 		assertion(caseinfo, response)
 		logger.info(f"{'接口请求结束':-^20}\n")
+	return wapper
+
+@caseHandle
+def autoSendRequest(caseinfo):
+	""" 获取用例自动发送请求 """
+	temp = dict(
+		url=caseinfo["request"].pop("url"),
+		method=caseinfo["request"].pop("method"),
+		base_url=caseinfo["base_url"],
+		files= caseinfo["request"].pop("files") if caseinfo["request"].get("files") else None
+	)
+	response = send_request(**temp,**caseinfo["request"])
+	return response
+
+def render_template(data):
+	""" 渲染用例 """
+	data = json.dumps(data) if data and isinstance(data, dict) else data
+	if extractPool and data:		# extract.yaml文件可能为空，为空时不渲染用例
+		temp = Template(data).render(**extractPool)
+		return yaml.load(stream=temp, Loader=yaml.FullLoader)
+	elif not data:
+		return None
+	else:
+		return yaml.load(stream=data, Loader=yaml.FullLoader)
+
+def parameterHandle(func):
+	""" 参数处理 """
+	def wapper(method, url, base_url=None, files=None,**kwargs):
+		method = str(method).lower()
+		url = render_template(url)
+		url = base_url + url if base_url else url
+		# 配置中无base_url或者send_request方法不传入base_url参数，都会使base_url为None
+		for key, value in kwargs.items():
+			if key in ("headers", "params", "data", "json"):
+				kwargs[key] = render_template(value)
+		if files:
+			for k, v in files.items():
+				files[k] = open(v, "rb")
+		return func(method,url,files,**kwargs)
+	return wapper
+
+def downloadFixture(func):
+	""" 文件下载 """
+	def wapper(method, url,files=None,**kwargs):
+		response = func(method, url,files,**kwargs)
+		download(response,["image/jpeg", "image/png", "application/pdf"])
+		return response
+	return wapper
+
+def logFixture(func):
+	""" 日志记录 """
+	def wapper(method, url,files=None,**kwargs):
+		logger.info(f"请求url:{url}")
+		logger.info(f"请求方法:{method}")
+		logger.info(f"请求参数:{kwargs}")
+		logger.info(f"文件上传:{files}")
+		response = func(method, url, files, **kwargs)
+		return response
+	return wapper
+
+def allureFixture(func):
+	""" allure记录 """
+	def wapper(method, url, files=None, **kwargs):
+		allure.attach(body=url, name="请求url:", attachment_type=allure.attachment_type.TEXT)
+		allure.attach(body=method, name="请求方式:", attachment_type=allure.attachment_type.TEXT)
+		allure.attach(body=json.dumps(kwargs, ensure_ascii=False), name="请求参数:",
+					  attachment_type=allure.attachment_type.TEXT)
+		if files:
+			allure.attach(body=json.dumps(files, ensure_ascii=False), name="文件上传:",
+						  attachment_type=allure.attachment_type.TEXT)
+		response = func(method, url, files, **kwargs)
+		allure.attach(body=str(response.status_code), name="响应状态码:", attachment_type=allure.attachment_type.TEXT)
+		try:
+			data = json.dumps(response.json(), ensure_ascii=False)
+		except Exception:
+			response.encoding = "utf-8"
+			data = response.text
+		finally:
+			allure.attach(body=data, name="响应数据:", attachment_type=allure.attachment_type.TEXT)
+		return response
+	return wapper
+
+@parameterHandle
+@downloadFixture
+@allureFixture
+@logFixture
+def send_request(method, url,files=None,**kwargs):
+	""" 发送同一个session请求 """
+	response = session.request(method=method, url=url, files=files,timeout=10,**kwargs)
 	return response
 
 def download(response, target):
@@ -151,45 +182,53 @@ def assertion(caseinfo, string):
 	if isinstance(validata, dict):
 		for key, value in validata.items():
 			if key == "equal" and isinstance(value, dict):		# 相等断言
-					for i, j in value.items():
-						if j:
-							if i == "status_code":		# 响应状态断言
-								logger.info(f"预期结果状态码:{j}")
-								logger.info(f"实际结果状态码:{string.status_code}")
-								logger.info(f"相等断言:断言{'通过' if j == string.status_code else '失败'}")
-								assert j == string.status_code, f"断言失败，预期结果状态码{j}不等于实际结果状态码{string.status_code}"
-							else:			# 响应状态断言
-								results = jsonpath.jsonpath(string.json(), "$..%s" % i)
-								if results:
-									if isinstance(j, str):		# 结果是个字符串
-										for x in results:
-											logger.info(f"预期结果:{j}")
-											logger.info(f"实际结果:{x}")
-											logger.info(f"相等断言:断言{'通过' if x == j else '失败'}")
-											assert x == j, f"断言失败，预期结果{j}不等于实际结果{x}"
-									elif isinstance(j, list):			# 结果是个列表
-										logger.info(f"预期结果:{j}")
-										logger.info(f"实际结果:{results}")
-										seq = list(map(lambda a, b: True if a == b else False, j, results))
-										assert all(seq), f"断言失败，预期结果{j}不等于实际结果{results}"
-										logger.info(f"相等断言:断言{'通过' if all(seq) else '失败'}")
-									else:
-										raise Exception(f"validata中{i}只能是字符串或列表") from None
-								else:
-									raise Exception(f"在响应中未找到{i}") from None
+				equalFrame(value,string)
 			elif key == "contain":
-				try:
-					data: dict = string.json()
-					temp: str = json.dumps(data, ensure_ascii=False)
-				except Exception:
-					string.encoding = "utf-8"
-					data: str = string.text
-					temp: str = data
-				finally:
-					if validata["contain"]:
-						seq = []
-						for x in validata["contain"]:
-							assert temp.find(x) != -1, f"断言失败，响应结果中未找到{x}"
-							message = f"{x + '找到，断言通过' if temp.find(x) != -1 else x + '未找到，断言失败'}"
-							seq.append(message)
-						logger.info("包含断言:" + ",".join(seq))
+				containFrame(validata["contain"],string)		# 包含断言
+
+def equalFrame(target,string):
+	""" 相等断言 """
+	for i, j in target.items():
+		if j:
+			if i == "status_code":  # 响应状态断言
+				logger.info(f"预期结果状态码:{j}")
+				logger.info(f"实际结果状态码:{string.status_code}")
+				logger.info(f"相等断言:断言{'通过' if j == string.status_code else '失败'}")
+				assert j == string.status_code, f"断言失败，预期结果状态码{j}不等于实际结果状态码{string.status_code}"
+			else:  # 响应状态断言
+				results = jsonpath.jsonpath(string.json(), "$..%s" % i)
+				if results:
+					if isinstance(j, str):  # 结果是个字符串
+						for x in results:
+							logger.info(f"预期结果:{j}")
+							logger.info(f"实际结果:{x}")
+							logger.info(f"相等断言:断言{'通过' if x == j else '失败'}")
+							assert x == j, f"断言失败，预期结果{j}不等于实际结果{x}"
+					elif isinstance(j, list):  # 结果是个列表
+						logger.info(f"预期结果:{j}")
+						logger.info(f"实际结果:{results}")
+						seq = list(map(lambda a, b: True if a == b else False, j, results))
+						assert all(seq), f"断言失败，预期结果{j}不等于实际结果{results}"
+						logger.info(f"相等断言:断言{'通过' if all(seq) else '失败'}")
+					else:
+						raise Exception(f"validata中{i}只能是字符串或列表") from None
+				else:
+					raise Exception(f"在响应中未找到{i}") from None
+
+def containFrame(target,string):
+	""" 包含断言 """
+	try:
+		data: dict = string.json()
+		temp: str = json.dumps(data, ensure_ascii=False)
+	except Exception:
+		string.encoding = "utf-8"
+		data: str = string.text
+		temp: str = data
+	finally:
+		if target:
+			seq = []
+			for x in target:
+				assert temp.find(x) != -1, f"断言失败，响应结果中未找到{x}"
+				message = f"{x + '找到，断言通过' if temp.find(x) != -1 else x + '未找到，断言失败'}"
+				seq.append(message)
+			logger.info("包含断言:" + ",".join(seq))
