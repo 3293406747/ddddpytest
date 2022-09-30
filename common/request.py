c80b8f62fb32c66c975085a64b3859358caeebe1
py85329140 <3293406747@qq.com>
2022-09-30 11:51:07 +0800

    优化代码结构

--- common/request.py
+++ common/request.py
@@ -16,19 +16,17 @@ path = Path(__file__).resolve()
 session = requests.session()
 extractPool = {}
 
-def caseHandle(func):
-	""" 用例处理 """
-	def wapper(caseinfo, start=None):
-		logger.info(f"{'接口请求开始':-^20}")
-		logger.info(f"请求名称:{caseinfo['name']}")
+
+def sqlHandle(func):
+	""" sql处理 """
+
+	def wapper(caseinfo):
 		response = func(caseinfo)
-		extract_variable(response, caseinfo, start=start)
-		caseinfo["validata"] = render_template(caseinfo["validata"])
 		if caseinfo["validata"]:
 			p = r'select\s*(.*?)\sfrom'
 			if caseinfo["validata"].get('equal'):
-				for k,v in caseinfo["validata"]["equal"].items():
-					if re.match(p,str(v)):
+				for k, v in caseinfo["validata"]["equal"].items():
+					if re.match(p, str(v)):
 						res = Mysql().select(sql=v)
 						key = re.match(p, v).group(1)
 						caseinfo["validata"]["equal"][k] = res[0][key]
@@ -40,26 +38,73 @@ def caseHandle(func):
 						key = re.match(p, i).group(1)
 						caseinfo["validata"]['contain'][num] = res[0][key]
 						num += 1
-		assertion(caseinfo, response)
-		logger.info(f"{'接口请求结束':-^20}\n")
+		return response
+
 	return wapper
 
-@caseHandle
+
+def extract_variable(func):
+	""" 接口关联:提取响应中的内容 """
+
+	def wapper(caseinfo):
+		response = func(caseinfo)
+		if "extract" in caseinfo.keys():
+			for key, value in caseinfo["extract"].items():
+				tf_list = list(map(lambda x: True if x in value else False, ["$", ">", "(", ")"]))
+				if all(tf_list):  # json+正则提取器
+					json_extract, re_extract = str(response.json()).split(">")
+					temp = jsonpath.jsonpath(str(json_extract).strip(), value)[0]
+					extract = re.search(str(re_extract).strip(), temp).group(1)
+				elif "(" in value and ")" in value:  # 正则提取器
+					extract = re.search(value, response.text).group(1)
+				elif "$" in value:  # json提取器
+					extract = jsonpath.jsonpath(response.json(), value)[0]
+				else:
+					raise Exception("提取器表达式错误") from None
+				extractPool[key] = extract
+		# 渲染validata
+		caseinfo["validata"] = render_template(caseinfo["validata"])
+		return response
+
+	return wapper
+
+
+def assertion(func):
+	""" 响应断言 """
+
+	def wapper(caseinfo):
+		response = func(caseinfo)
+		validata = caseinfo["validata"]
+		if isinstance(validata, dict):
+			for key, value in validata.items():
+				if key == "equal" and isinstance(value, dict):  # 相等断言
+					equalFrame(value, response)
+				elif key == "contain":
+					containFrame(validata["contain"], response)  # 包含断言
+
+	return wapper
+
+
+@assertion
+@sqlHandle
+@extract_variable
 def autoSendRequest(caseinfo):
 	""" 获取用例自动发送请求 """
 	temp = dict(
 		url=caseinfo["request"].pop("url"),
 		method=caseinfo["request"].pop("method"),
 		base_url=caseinfo["base_url"],
-		files= caseinfo["request"].pop("files") if caseinfo["request"].get("files") else None
+		files=caseinfo["request"].pop("files") if caseinfo["request"].get("files") else None,
+		name=caseinfo["name"]
 	)
-	response = send_request(**temp,**caseinfo["request"])
+	response = send_request(**temp, **caseinfo["request"])
 	return response
+
 
 def render_template(data):
 	""" 渲染用例 """
 	data = json.dumps(data) if data and isinstance(data, dict) else data
-	if extractPool and data:		# extract.yaml文件可能为空，为空时不渲染用例
+	if extractPool and data:  # extract.yaml文件可能为空，为空时不渲染用例
 		temp = Template(data).render(**extractPool)
 		return yaml.load(stream=temp, Loader=yaml.FullLoader)
 	elif not data:
@@ -67,9 +112,11 @@ def render_template(data):
 	else:
 		return yaml.load(stream=data, Loader=yaml.FullLoader)
 
+
 def parameterHandle(func):
 	""" 参数处理 """
-	def wapper(method, url, base_url=None, files=None,**kwargs):
+
+	def wapper(method, url, base_url=None, files=None, **kwargs):
 		method = str(method).lower()
 		url = render_template(url)
 		url = base_url + url if base_url else url
@@ -80,30 +127,58 @@ def parameterHandle(func):
 		if files:
 			for k, v in files.items():
 				files[k] = open(v, "rb")
-		return func(method,url,files,**kwargs)
+		return func(method, url, files, **kwargs)
+
 	return wapper
+
 
 def downloadFixture(func):
 	""" 文件下载 """
-	def wapper(method, url,files=None,**kwargs):
-		response = func(method, url,files,**kwargs)
-		download(response,["image/jpeg", "image/png", "application/pdf"])
+
+	def wapper(method, url, files=None, **kwargs):
+		response = func(method, url, files, **kwargs)
+		flag = False
+		if isinstance(response.content, bytes) and response.headers.get("Content-Type"):
+			ct = response.headers["Content-Type"]
+			if ct == "image/jpeg":
+				flag = True
+				allure.attach(body=response.content, name="jpeg图片", attachment_type=allure.attachment_type.JPG)
+			elif ct == "image/png":
+				flag = True
+				allure.attach(body=response.content, name="png图片", attachment_type=allure.attachment_type.PNG)
+			elif ct == "application/pdf":
+				flag = True
+				allure.attach(body=response.content, name="pdf", attachment_type=allure.attachment_type.PDF)
+			if flag:
+				path.parent.parent.joinpath('data').mkdir(parents=True, exist_ok=True)
+				file = Path(path.parent.parent, 'data', (str(int(time.time())) + ".%s") % ct.split("/")[1])
+				file.open(mode="wb").write(response.content)
+				logger.info(f"{ct.split('/')[1]}格式文件下载成功，文件下载路径:{file}")
 		return response
+
 	return wapper
+
 
 def logFixture(func):
 	""" 日志记录 """
-	def wapper(method, url,files=None,**kwargs):
+
+	def wapper(method, url, files=None, name=None, **kwargs):
+		logger.info(f"{'接口请求开始':-^20}")
+		logger.info(f"请求名称:{name}")
 		logger.info(f"请求url:{url}")
 		logger.info(f"请求方法:{method}")
 		logger.info(f"请求参数:{kwargs}")
 		logger.info(f"文件上传:{files}")
 		response = func(method, url, files, **kwargs)
+		logger.info(f"{'接口请求结束':-^20}\n")
 		return response
+
 	return wapper
+
 
 def allureFixture(func):
 	""" allure记录 """
+
 	def wapper(method, url, files=None, **kwargs):
 		allure.attach(body=url, name="请求url:", attachment_type=allure.attachment_type.TEXT)
 		allure.attach(body=method, name="请求方式:", attachment_type=allure.attachment_type.TEXT)
@@ -114,6 +189,7 @@ def allureFixture(func):
 						  attachment_type=allure.attachment_type.TEXT)
 		response = func(method, url, files, **kwargs)
 		allure.attach(body=str(response.status_code), name="响应状态码:", attachment_type=allure.attachment_type.TEXT)
+		data = None
 		try:
 			data = json.dumps(response.json(), ensure_ascii=False)
 		except Exception:
@@ -122,71 +198,20 @@ def allureFixture(func):
 		finally:
 			allure.attach(body=data, name="响应数据:", attachment_type=allure.attachment_type.TEXT)
 		return response
+
 	return wapper
+
 
 @parameterHandle
 @downloadFixture
 @allureFixture
 @logFixture
-def send_request(method, url,files=None,**kwargs):
+def send_request(method, url, files=None, **kwargs):
 	""" 发送同一个session请求 """
-	response = session.request(method=method, url=url, files=files,timeout=10,**kwargs)
+	response = session.request(method=method, url=url, files=files, timeout=10, **kwargs)
 	return response
 
-def download(response, target):
-	""" 文件下载 """
-	if response.headers.get("Content-Type"):
-		tp = target.pop()
-		if tp in response.headers["Content-Type"]:
-			path.parent.parent.joinpath('data').mkdir(parents=True,exist_ok=True)
-			file = Path(path.parent.parent,'data',(str(int(time.time())) + ".%s") % tp.split("/")[1])
-			with open(file=file, mode="wb") as f:
-				f.write(response.content)
-			logger.info(f"{tp.split('/')[1]}格式文件下载成功，文件下载路径:{file}")
-			with open(file, mode="rb") as f:
-				if tp == "image/jpeg":
-					allure.attach(body=f.read(), name="jpeg图片", attachment_type=allure.attachment_type.JPG)
-				elif tp == "image/png":
-					allure.attach(body=f.read(), name="png图片", attachment_type=allure.attachment_type.PNG)
-				elif tp == "application/pdf":
-					allure.attach(body=f.read(), name="pdf", attachment_type=allure.attachment_type.PDF)
-			return None
-		elif not target:
-			return None
-		else:
-			return download(response, target)
-
-def extract_variable(string, case_info, start=None):
-	""" 接口关联:提取响应中的内容 """
-	if "extract" in case_info.keys():
-		for key, value in case_info["extract"].items():
-			tf_list = list(map(lambda x: True if x in value else False, ["$", ">", "(", ")"]))
-			if all(tf_list):		# json+正则提取器
-				json_extract, re_extract = str(string.json()).split(">")
-				temp = jsonpath.jsonpath(str(json_extract).strip(), value)[0]
-				response = re.search(str(re_extract).strip(), temp).group(1)
-			elif "(" in value and ")" in value:			# 正则提取器
-				response = re.search(value, string.text).group(1)
-			elif "$" in value:		# json提取器
-				response = jsonpath.jsonpath(string.json(), value)[0]
-			else:
-				raise Exception("提取器表达式错误") from None
-			if start:
-				response = start + response
-			extractPool[key] = response
-
-
-def assertion(caseinfo, string):
-	""" 响应断言 """
-	validata = caseinfo["validata"]
-	if isinstance(validata, dict):
-		for key, value in validata.items():
-			if key == "equal" and isinstance(value, dict):		# 相等断言
-				equalFrame(value,string)
-			elif key == "contain":
-				containFrame(validata["contain"],string)		# 包含断言
-
-def equalFrame(target,string):
+def equalFrame(target, string):
 	""" 相等断言 """
 	for i, j in target.items():
 		if j:
@@ -215,8 +240,10 @@ def equalFrame(target,string):
 				else:
 					raise Exception(f"在响应中未找到{i}") from None
 
-def containFrame(target,string):
+
+def containFrame(target, string):
 	""" 包含断言 """
+	temp = ""
 	try:
 		data: dict = string.json()
 		temp: str = json.dumps(data, ensure_ascii=False)
