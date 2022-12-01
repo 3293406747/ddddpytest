353c0fb99a91146e52327af2938eaf526ed0db95
py85329140 <3293406747@qq.com>
2022-12-01 15:19:13 +0800

    优化代码结构。

--- common/assertion.py
+++ common/assertion.py
@@ -4,86 +4,92 @@ from common.logger import logger
 
 class Assertion:
 	""" 断言 """
+
 	@classmethod
-	def equal(cls,expect,actual,name=None):
+	def equal(cls, expect, actual, name=None):
 		""" 相等断言 """
-		return Factory.create(method="equal",expect=expect,actual=actual,name=name)
+		return Factory.create(method="equal", expect=expect, actual=actual, name=name)
 
 	@classmethod
-	def unequal(cls,expect,actual,name=None):
+	def unequal(cls, expect, actual, name=None):
 		""" 不相等断言 """
-		return Factory.create(method="unequal",expect=expect,actual=actual,name=name)
+		return Factory.create(method="unequal", expect=expect, actual=actual, name=name)
 
 	@classmethod
-	def contian(cls,expect,actual,name=None):
+	def contian(cls, expect, actual, name=None):
 		""" 包含断言 """
-		return Factory.create(method="contain",expect=expect,actual=actual,name=name)
+		return Factory.create(method="contain", expect=expect, actual=actual, name=name)
 
 	@classmethod
-	def uncontian(cls,expect,actual,name=None):
+	def uncontian(cls, expect, actual, name=None):
 		""" 不包含断言 """
-		return Factory.create(method="uncontain",expect=expect,actual=actual,name=name)
+		return Factory.create(method="uncontain", expect=expect, actual=actual, name=name)
 
 
 class Factory:
 
 	@classmethod
-	def create(cls,method,expect,actual,name=None):
-		if method in ["equal","unequal"]:
-			if isinstance(expect,(str, int, float)):
-				expect = [expect]
-			if isinstance(actual,(str, int, float)):
-				actual = [actual]
-			# list|tuple|set
-			if isinstance(expect, (list, tuple, set)) and isinstance(actual, (list, tuple, set)):
-				for i in range(len(expect)):
-					try:
-						# 相等断言
-						if method == "equal":
-							assert expect[i] == actual[i]
-							msg = f"{name or ''}断言通过:{str(expect[i])}等于{str(actual[i])}"
-						# 不相等断言
-						else:
-							assert expect[i] != actual[i]
-							msg = f"{name or ''}断言通过:{str(expect[i])}不等于{str(actual[i])}"
-						logger.success(msg)
-					except AssertionError:
-						if method == "equal":
-							msg = f"{name or ''}断言失败:{str(expect[i])}不等于{str(actual[i])}"
-						else:
-							msg = f"{name or ''}断言失败:{str(expect[i])}等于{str(actual[i])}"
-						logger.error(msg)
-						raise AssertionError(msg) from None
-			else:
-				msg = f"{expect}与{actual}类型不一致或类型格式不支持。"
-				raise Exception(msg)
-		elif method in ["contain","uncontain"]:
+	def create(cls, method, expect, actual, name=None):
+		if method in ["equal", "unequal"]:
+			list(map(lambda x: [x] if isinstance(x, (str, int, float)) else x, [expect, actual]))
+			Method.equal_unqual(method=method, expect=expect, actual=actual, name=name)
+		elif method in ["contain", "uncontain"]:
 			actual = json.dumps(actual, ensure_ascii=False) if isinstance(actual, dict) else actual
-			# list|tuple|set
-			if isinstance(expect,str):
-				expect = [expect]
-			if isinstance(expect, (list, tuple, set)):
-				for i in expect:
-					try:
-						# 包含断言
-						if method == "contain":
-							assert i in actual
-							msg = f"{name or ''}断言通过:{str(i)}在{actual:.255s}中存在"
-						# 不包含断言
-						else:
-							assert i not in actual
-							msg = f"{name or ''}断言通过:{str(i)}在{actual:.255s}中不存在"
-						logger.success(msg)
-					except AssertionError:
-						if method == "contain":
-							msg = f"{name or ''}断言失败:{str(i)}在{actual:.255s}中不存在"
-						else:
-							msg = f"{name or ''}断言失败:{str(i)}在{actual:.255s}中存在"
-						logger.error(msg)
-						raise AssertionError(msg) from None
-			else:
-				msg = f"{expect}类型格式不支持。"
-				raise Exception(msg)
+			expect = [expect] if isinstance(expect, str) else expect
+			Method.contain_uncontain(method=method, expect=expect, actual=actual, name=name)
 		else:
 			msg = "断言格式不支持。"
 			raise Exception(msg)
+
+
+class Method:
+
+	@classmethod
+	def equal_unqual(cls, method, expect, actual, name):
+		# list|tuple|set
+		if not isinstance(expect, (list, tuple, set)) or not isinstance(actual, (list, tuple, set)):
+			msg = f"{expect}与{actual}类型不一致或类型格式不支持。"
+			raise Exception(msg)
+		for i in range(len(expect)):
+			try:
+				# 相等断言
+				if method == "equal":
+					assert expect[i] == actual[i]
+					msg = f"{name or ''}断言通过:{str(expect[i])}等于{str(actual[i])}"
+				# 不相等断言
+				else:
+					assert expect[i] != actual[i]
+					msg = f"{name or ''}断言通过:{str(expect[i])}不等于{str(actual[i])}"
+				logger.success(msg)
+			except AssertionError:
+				if method == "equal":
+					msg = f"{name or ''}断言通过:{str(expect[i])}不等于{str(actual[i])}"
+				else:
+					msg = f"{name or ''}断言失败:{str(expect[i])}等于{str(actual[i])}"
+				logger.error(msg)
+				raise AssertionError(msg) from None
+
+	@classmethod
+	def contain_uncontain(cls, method, expect, actual, name):
+		# list|tuple|set
+		if not isinstance(expect, (list, tuple, set)):
+			msg = f"{expect}类型格式不支持。"
+			raise Exception(msg)
+		for i in expect:
+			try:
+				# 包含断言
+				if method == "contain":
+					assert i in actual
+					msg = f"{name or ''}断言通过:{str(i)}在{actual:.255s}中存在"
+				# 不包含断言
+				else:
+					assert i not in actual
+					msg = f"{name or ''}断言通过:{str(i)}在{actual:.255s}中不存在"
+				logger.success(msg)
+			except AssertionError:
+				if method == "contain":
+					msg = f"{name or ''}断言失败:{str(i)}在{actual:.255s}中不存在"
+				else:
+					msg = f"{name or ''}断言失败:{str(i)}在{actual:.255s}中存在"
+				logger.error(msg)
+				raise AssertionError(msg) from None
--- common/case.py
+++ common/case.py
@@ -15,126 +15,157 @@ def renderTemplate(data):
 	# 使用变量
 	merge = {**Variables().pool, **Globals().pool, **Environment().pool}
 	# merge = Variables().pool | Globals().pool | Environment().pool
-	data = renderVariables(obj=data, mapping=merge)
+	data = Factory.create(method="vary",obj=data,mapping=merge)
 	data = Template(json.dumps(data,ensure_ascii=False)).safe_substitute(merge)
 	# 调用python函数
-	data = renderFunction(data)
+	data = Factory.create(method="func", obj=data, mapping=merge)
 	data = pattern.sub(repl=parse, string=json.dumps(data,ensure_ascii=False))
 	return json.loads(data)
 
-def renderVariables(obj,mapping:dict):
-	""" 使用变量 """
-	if isinstance(obj,str):
-		obj = json.loads(obj)
-	if isinstance(obj,list):
-		for key,value in enumerate(obj):
-			if isinstance(value,str):
-				res = re.match(r"^\$\{(.*?)\}$", value)
-				if res and mapping.get(res.group(1)):
-					obj[key] = mapping.get(res.group(1))
-			elif isinstance(value,(list,dict)):
-				obj[key] = renderVariables(value, mapping)
-	elif isinstance(obj,dict):
-		for key,value in obj.items():
-			if isinstance(value,str):
-				res = re.match(r"^\$\{(.*?)\}$", value)
-				if res and mapping.get(res.group(1)):
-					obj[key] = mapping.get(res.group(1))
-			elif isinstance(value,(list,dict)):
-				obj[key] = renderVariables(value, mapping)
-	return obj
 
-def renderFunction(obj):
-	""" 调用python函数 """
-	if isinstance(obj,str):
-		obj = json.loads(obj)
-	if isinstance(obj,list):
-		for key,value in enumerate(obj):
-			if isinstance(value,str):
-				res = re.match(r"^\{\{(.*?)\}\}$", value)
-				if res:
-					obj[key] = parse(res)
-			elif isinstance(value,(list,dict)):
-				obj[key] = renderFunction(value)
-	elif isinstance(obj,dict):
-		for key,value in obj.items():
-			if isinstance(value,str):
-				res = re.match(r"^\{\{(.*?)\}\}$", value)
-				if res:
-					obj[key] = parse(res)
-			elif isinstance(value,(list,dict)):
-				obj[key] = renderFunction(value)
-	return obj
+class Factory:
+
+	@classmethod
+	def create(cls,method,obj,mapping=None):
+		if isinstance(obj, str):
+			obj = json.loads(obj)
+		if isinstance(obj, list):
+			for key, value in enumerate(obj):
+				if method == "vary":
+					Method.vary(key=key,value=value,mapping=mapping,obj=obj)
+				elif method == "func":
+					Method.func(key=key,value=value,obj=obj)
+				else:
+					msg = "method not found"
+					raise Exception(msg)
+		elif isinstance(obj, dict):
+			for key, value in obj.items():
+				if method == "vary":
+					Method.vary(key=key,value=value,mapping=mapping,obj=obj)
+				elif method == "func":
+					Method.func(key=key,value=value,obj=obj)
+				else:
+					msg = "method not found"
+					raise Exception(msg)
+		return obj
+
+
+class Method:
+
+	@classmethod
+	def vary(cls,key,value,mapping,obj):
+		""" 使用变量 """
+		if isinstance(value, str):
+			res = re.match(r"^\$\{(.*?)\}$", value)
+			if res and mapping.get(res.group(1)):
+				obj[key] = mapping.get(res.group(1))
+		elif isinstance(value, (list, dict)):
+			obj[key] = Factory.create(method="vary",obj=value,mapping=mapping)
+
+	@classmethod
+	def func(cls,key,value,obj):
+		""" 调用python函数 """
+		if isinstance(value, str):
+			res = re.match(r"^\{\{(.*?)\}\}$", value)
+			if res:
+				obj[key] = parse(res)
+		elif isinstance(value, (list, dict)):
+			obj[key] = Factory.create(method="func",obj=value)
+
+
+class VerifyCase:
+	""" 校验用例格式 """
+	def __init__(self,case):
+		self.case_new = copy.deepcopy(case)
+		self.case_str = json.dumps(case,ensure_ascii=False)
+
+	def args_must(self):
+		# 必选参数校验
+		for key in ["casename", "request"]:
+			if not self.case_new.get(key):
+				msg = f"{self.case_str:.255s}必须包含一级关键字casename,request"
+				raise Exception(msg)
+		self.case_new.pop("casename")
+
+	def request(self):
+		request = self.case_new.pop("request")
+		# request校验
+		for key in ["url", "method"]:
+			if not request.get(key):
+				msg = f"{self.case_str:.255s}的request关键字下必须包含二级关键字url,method"
+				raise Exception(msg)
+			else:
+				request.pop(key)
+		requestOtherKeys = ["params", "data", "json", "files", "headers"]
+		for i in request.keys():
+			if i not in requestOtherKeys:
+				msg = f"{self.case_str:.255s}的request关键字下不能包含除{','.join(requestOtherKeys)}之外的关键字。"
+				raise Exception(msg)
+
+	def args_unmust(self):
+		# 非必选参数校验
+		otherKeys = ["data_path", "extract", "assertion", "session"]
+		for i in self.case_new.keys():
+			if i not in otherKeys:
+				msg = f"{self.case_str:.255s}不能包含除casename,request,data_path,extract,assertion,session之外的一级关键字。"
+				raise Exception(msg)
+
+	def extract(self):
+		# extract校验
+		if self.case_new.get("extract"):
+			extractKeys = ["request", "response"]
+			if not isinstance(self.case_new.get("extract"), dict):
+				msg = f"{self.case_str:.255s}的extract关键字下必须为字典格式。"
+				raise Exception(msg)
+			for key, value in self.case_new.get("extract").items():
+				if key not in extractKeys:
+					msg = f"{self.case_str:.255s}的extract关键字下不能包含除{','.join(extractKeys)}之外的关键字。"
+					raise Exception(msg)
+				if not isinstance(value, dict):
+					msg = f"{self.case_str:.255s}的extract关键字下的{key}必须为字典格式。"
+					raise Exception(msg)
+
+	def session(self):
+		# session校验
+		if self.case_new.get("session") and not isinstance(self.case_new.get("session"), int):
+			msg = f"{self.case_str:.255s}的session关键字下必须整数格式。"
+			raise Exception(msg)
+
+	def assertion(self):
+		# assertion校验
+		if self.case_new.get("assertion") and isinstance(self.case_new.get("assertion"), dict):
+			assertionKeys = ["contain", "uncontain", "equal", "unequal"]
+			for i in self.case_new.get("assertion").keys():
+				if i not in assertionKeys:
+					msg = f"{self.case_str:.255s}的assertion关键字下不能包含除{','.join(assertionKeys)}之外的关键字。"
+					raise Exception(msg)
+				elif i in ["equal", "unequal"]:
+					for key in ["expect", "actual"]:
+						if not isinstance(self.case_new["assertion"][i], list):
+							msg = f"{self.case_str:.255s}的assertion关键字下的{i}必须是list格式。"
+							raise Exception(msg)
+						for j in self.case_new["assertion"][i]:
+							if key not in j.keys():
+								msg = f"{self.case_str:.255s}的assertion关键字下的{i}关键字下必须包含expect,actual关键字。"
+								raise Exception(msg)
+				elif i in ["contain", "uncontain"]:
+					if not isinstance(self.case_new["assertion"][i], list):
+						msg = f"{self.case_str:.255s}的assertion关键字下的{i}关键字必须为list格式。"
+						raise Exception(msg)
+		elif self.case_new.get("assertion") and not isinstance(self.case_new.get("assertion"), dict):
+			msg = f"{self.case_str:.255s}的assertion关键字必须是字典格式。"
+			raise Exception(msg)
+
 
 def verifyCase(case):
 	""" 校验用例格式 """
-	newCase: dict = copy.deepcopy(case)
-	strcase = json.dumps(case, ensure_ascii=False)
-	# 必选参数校验
-	for key in ["casename", "request"]:
-		if not newCase.get(key):
-			msg = f"{strcase:.255s}必须包含一级关键字casename,request"
-			raise Exception(msg)
-	newCase.pop("casename")
-	request = newCase.pop("request")
-	# request校验
-	for key in ["url", "method"]:
-		if not request.get(key):
-			msg = f"{strcase:.255s}的request关键字下必须包含二级关键字url,method"
-			raise Exception(msg)
-		else:
-			request.pop(key)
-	requestOtherKeys = ["params", "data", "json", "files","headers"]
-	for i in request.keys():
-		if i not in requestOtherKeys:
-			msg = f"{strcase:.255s}的request关键字下不能包含除{','.join(requestOtherKeys)}之外的关键字。"
-			raise Exception(msg)
-	# 非必选参数校验
-	otherKeys = ["data_path", "extract", "assertion", "session"]
-	for i in newCase.keys():
-		if i not in otherKeys:
-			msg = f"{strcase:.255s}不能包含除casename,request,data_path,extract,assertion,session之外的一级关键字。"
-			raise Exception(msg)
-	# extract校验
-	if newCase.get("extract"):
-		extractKeys = ["request", "response"]
-		if not isinstance(newCase.get("extract"), dict):
-			msg = f"{strcase:.255s}的extract关键字下必须为字典格式。"
-			raise Exception(msg)
-		for key, value in newCase.get("extract").items():
-			if key not in extractKeys:
-				msg = f"{strcase:.255s}的extract关键字下不能包含除{','.join(extractKeys)}之外的关键字。"
-				raise Exception(msg)
-			if not isinstance(value, dict):
-				msg = f"{strcase:.255s}的extract关键字下的{key}必须为字典格式。"
-				raise Exception(msg)
-	# session校验
-	if newCase.get("session") and not isinstance(newCase.get("session"), int):
-		msg = f"{strcase:.255s}的session关键字下必须整数格式。"
-		raise Exception(msg)
-	# assertion校验
-	if newCase.get("assertion") and isinstance(newCase.get("assertion"), dict):
-		assertionKeys = ["contain", "uncontain", "equal", "unequal"]
-		for i in newCase.get("assertion").keys():
-			if i not in assertionKeys:
-				msg = f"{strcase:.255s}的assertion关键字下不能包含除{','.join(assertionKeys)}之外的关键字。"
-				raise Exception(msg)
-			elif i in ["equal", "unequal"]:
-				for key in ["expect", "actual"]:
-					if not isinstance(newCase["assertion"][i], list):
-						msg = f"{strcase:.255s}的assertion关键字下的{i}必须是list格式。"
-						raise Exception(msg)
-					for j in newCase["assertion"][i]:
-						if key not in j.keys():
-							msg = f"{strcase:.255s}的assertion关键字下的{i}关键字下必须包含expect,actual关键字。"
-							raise Exception(msg)
-			elif i in ["contain", "uncontain"]:
-				if not isinstance(newCase["assertion"][i], list):
-					msg = f"{strcase:.255s}的assertion关键字下的{i}关键字必须为list格式。"
-					raise Exception(msg)
-	elif newCase.get("assertion") and not isinstance(newCase.get("assertion"), dict):
-		msg = f"{strcase:.255s}的assertion关键字必须是字典格式。"
-		raise Exception(msg)
+	ver = VerifyCase(case=case)
+	ver.args_must()
+	ver.request()
+	ver.args_unmust()
+	ver.extract()
+	ver.session()
+	ver.assertion()
 	return case
 
 
