6e5e94fd956567db22619f0d11fa3f5306449e77
py85329140 <3293406747@qq.com>
2023-02-04 17:36:44 +0800

    u

--- utils/extract.py
+++ utils/extract.py
@@ -2,38 +2,16 @@
 提取内容
 """
 import json, re, jsonpath
+from abc import ABCMeta,abstractmethod
 
+class Mode(metaclass=ABCMeta):
 
-class Extract:
-	""" 提取内容 """
+	@abstractmethod
+	def extract(self,data,pattern):
+		pass
 
-	@staticmethod
-	def json(data, pattern, index=None):
-		""" json提取 """
-		return Factory.create(method="json", data=data, pattern=pattern, index=index)
-
-	@staticmethod
-	def match(data, pattern, index=None):
-		""" 正则提取 """
-		return Factory.create(method="match", data=data, pattern=pattern, index=index)
-
-
-extract = Extract()
-
-
-class Factory:
-
-	@classmethod
-	def create(cls, method, data, pattern, index):
-		# json提取
-		if method == "json":
-			result = Type.json(data=data, pattern=pattern)
-		# 正则提取
-		elif method == "match":
-			result = Type.match(data=data, pattern=pattern)
-		else:
-			msg = "提取方式不支持。"
-			raise Exception(msg)
+	def execute(self,data,pattern,index):
+		result = self.extract(data,pattern)
 		if not result:
 			data = json.dumps(data, ensure_ascii=False) if isinstance(data, dict) else data
 			raise Exception(f"要提取的值{pattern}在{data}中不存在")
@@ -42,14 +20,35 @@ class Factory:
 		return result
 
 
-class Type:
-
-	@classmethod
-	def json(cls, data, pattern):
+class Json(Mode):
+	""" json提取 """
+	def extract(self,data,pattern):
 		obj = json.loads(data) if isinstance(data, str) else data
 		return jsonpath.jsonpath(obj=obj, expr=pattern)
 
-	@classmethod
-	def match(cls, data, pattern):
+class Match(Mode):
+	""" 正则提取 """
+	def extract(self,data,pattern):
 		obj = json.dumps(data, ensure_ascii=False) if isinstance(data, dict) else data
 		return re.findall(pattern=pattern, string=obj)
+
+json_ = Json()
+match_ = Match()
+
+
+class Extract:
+	""" 提取内容 """
+
+	@staticmethod
+	def json(data, pattern, index=None):
+		""" json提取 """
+		return json_.execute(data=data, pattern=pattern, index=index)
+
+	@staticmethod
+	def match(data, pattern, index=None):
+		""" 正则提取 """
+		return match_.execute(data=data, pattern=pattern, index=index)
+
+
+extract = Extract()
+
