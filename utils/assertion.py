4341b81c4e04bac7095cb7810c637c341a4a0698
py85329140 <3293406747@qq.com>
2023-03-06 21:35:02 +0800

    优化代码结构

--- utils/assertion.py
+++ utils/assertion.py
@@ -1,19 +1,30 @@
 """
 断言
 """
-import json
 from abc import ABCMeta, abstractmethod
-from utils.logger import logger
 import numbers
-# from functools import lru_cache
+from functools import partial
 
 
 class Mode(metaclass=ABCMeta):
 	""" 抽象基类，定义 execute 方法 """
 
 	@abstractmethod
-	def excute(self, expect, actual, name) -> None:
+	def excute(self, expect, actual) -> None:
 		pass
+
+	@staticmethod
+	def format_param(target) -> list:
+		"""转换为列表"""
+		if isinstance(target, (str,numbers.Number)):
+			return [target]
+		elif isinstance(target, (tuple, set)):
+			return list(target)
+		elif isinstance(target, list):
+			return target
+		else:
+			msg = f"不支持的类型{type(target)}"
+			raise TypeError(msg)
 
 
 class Equal(Mode):
@@ -22,59 +33,39 @@ class Equal(Mode):
 	def __init__(self, flag=False):
 		self.flag = flag
 
-	def excute(self, expect, actual, name) -> None:
+	def excute(self, expect, actual):
 		expect, actual = self.format_param(expect), self.format_param(actual)  # 转换为列表
-		self.check_len(expect, actual)  # 检查长度是否一致
+		msg = self.check_len(expect, actual)  # 检查长度是否一致
+		if msg:
+			return msg
+		assert_fn = partial(self.assert_not_equal if self.flag else self.assert_equal)
 		for expect_item, actual_item in zip(expect, actual):  # 对于 list 进行逐个元素的断言
-			if not self.flag:
-				self.assert_equal(expect_item, actual_item, name)  # 相等断言
-			else:
-				self.assert_not_equal(expect_item, actual_item, name)  # 不相等断言
+			msg = assert_fn(expect_item, actual_item)
+			if msg:
+				return msg
+		msg = "断言通过。"
+		return msg
 
 	@staticmethod
-	def format_param(target) -> list:
-		"""转换为列表"""
-		# tuple|set
-		if isinstance(target, (str, numbers.Number)):
-			return [target]
-		elif isinstance(target, (tuple, set)):
-			return list(target)
-		elif isinstance(target, list):
-			return target
-		else:
-			msg = f"不支持的类型{type(target)}"
-			raise TypeError(msg)
-
-	@staticmethod
-	def check_len(expect: list, actual: list) -> None:
+	def check_len(expect: list, actual: list):
 		"""检查预期结果与实际结果长度是否相等"""
 		if len(expect) != len(actual):
-			msg = f"预期结果列表长度{len(expect)}与实际结果列表长度{len(actual)}不一致"
-			raise AssertionError(msg)
+			msg = f"断言失败，预期结果列表长度{len(expect)}与实际结果列表长度{len(actual)}不一致"
+			return msg
 
 	@staticmethod
-	def assert_equal(expect, actual, name=None) -> None:
+	def assert_equal(expect, actual):
 		"""相等断言"""
-		try:
-			assert expect == actual
-			msg = f"{name or ''}断言通过:{expect}等于{actual}"
-			logger.success(msg)
-		except AssertionError:
-			msg = f"{name or ''}断言失败:{expect}不等于{actual}"
-			logger.error(msg)
-			raise AssertionError(msg) from None
+		if expect != actual:
+			msg = f"断言失败，{expect}不等于{actual}。"
+			return msg
 
 	@staticmethod
-	def assert_not_equal(expect, actual, name=None) -> None:
+	def assert_not_equal(expect, actual):
 		"""不相等断言"""
-		try:
-			assert expect != actual
-			msg = f"{name or ''}断言通过:{expect}不等于{actual}"
-			logger.success(msg)
-		except AssertionError:
-			msg = f"{name or ''}断言失败:{expect}等于{actual}"
-			logger.error(msg)
-			raise AssertionError(msg) from None
+		if expect == actual:
+			msg = f"断言失败，{expect}等于{actual}"
+			return msg
 
 
 class Contain(Mode):
@@ -83,68 +74,41 @@ class Contain(Mode):
 	def __init__(self, flag=False):
 		self.flag = flag
 
-	def excute(self, expect, actual, name) -> None:
-		actual = self.format_actual(actual)  # 转换为json格式
-		expect = self.format_expect(expect)  # 转换为列表
-		for i in expect:  # 对于 list 进行逐个元素的断言
-			if not self.flag:
-				self.assert_contain(i, actual, name)  # 包含断言
-			else:
-				self.assert_not_contain(i, actual, name)  # 不包含断言
+	def excute(self, expect, actual: str):
+		if not isinstance(actual,str):
+			raise TypeError(f"{actual}必须是字符串类型")
+		expect = self.format_param(expect)  # 转换为列表
+		assert_fn = partial(self.assert_not_contain if self.flag else self.assert_contain)
+		for expect_item in expect:  # 对于 list 进行逐个元素的断言
+			msg = assert_fn(expect_item, actual)
+			if msg:
+				return msg
+		msg = "断言通过"
+		return msg
 
 	@staticmethod
-	def format_actual(target):
-		"""转换为json格式"""
-		if isinstance(target, dict):
-			return json.dumps(target, ensure_ascii=False)
-		return target
-
-	@staticmethod
-	def format_expect(target) -> list:
-		"""转换为列表"""
-		if isinstance(target, str):
-			return [target]
-		elif isinstance(target, (tuple, set)):
-			return list(target)
-		elif isinstance(target, list):
-			return target
-		else:
-			msg = f"不支持的类型{type(target)}"
-			raise TypeError(msg)
-
-	@staticmethod
-	def assert_contain(expect, actual, name=None) -> None:
+	def assert_contain(expect, actual):
 		"""包含断言"""
-		try:
-			assert expect in actual
-			msg = f"{name or ''}断言通过:{expect}在{actual:.255s}中存在"
-			logger.success(msg)
-		except AssertionError:
-			msg = f"{name or ''}断言失败:{expect}在{actual:.255s}中不存在"
-			logger.error(msg)
-			raise AssertionError(msg) from None
+		if expect not in actual:
+			msg = f"断言失败:{expect}在实际结果中不存在"
+			return msg
 
 	@staticmethod
-	def assert_not_contain(expect, actual, name=None) -> None:
+	def assert_not_contain(expect, actual):
 		"""不包含断言"""
-		try:
-			assert expect not in actual
-			msg = f"{name or ''}断言通过:{expect}在{actual:.255s}中不存在"
-			logger.success(msg)
-		except AssertionError:
-			msg = f"{name or ''}断言失败:{expect}在{actual:.255s}中存在"
-			logger.error(msg)
-			raise AssertionError(msg) from None
+		if expect in actual:
+			msg = f"断言失败:{expect}在实际结果中存在"
+			return msg
 
 
 class AssertionStrategy:
 	"""策略类，根据传入的 strategy 执行相应的策略"""
 
-	def __init__(self,strategy:Mode):
+	def __init__(self, strategy: Mode):
 		self.strategy = strategy
 
-	def excute(self,expect,actual,name):
-		self.strategy.excute(expect,actual,name)
+	def excute(self, expect, actual):
+		return self.strategy.excute(expect, actual)
 
 
 class Assertion:
@@ -156,29 +120,28 @@ class Assertion:
 	not_contain_strategy = AssertionStrategy(Contain(True))
 
 	@classmethod
-	def equal(cls, expect, actual, name=None):
+	def equal(cls, expect, actual):
 		""" 相等断言 """
 		# return AssertionFactory.create(method="equal").excute(expect=expect, actual=actual, name=name)
-		return cls.equal_strategy.excute(expect=expect, actual=actual, name=name)
+		return cls.equal_strategy.excute(expect=expect, actual=actual)
 
 	@classmethod
-	def unequal(cls, expect, actual, name=None):
+	def unequal(cls, expect, actual):
 		""" 不相等断言 """
 		# return AssertionFactory.create(method="unequal").excute(expect=expect, actual=actual, name=name)
-		return cls.not_equal_strategy.excute(expect=expect, actual=actual, name=name)
+		return cls.not_equal_strategy.excute(expect=expect, actual=actual)
 
 	@classmethod
-	def contian(cls, expect, actual, name=None):
+	def contian(cls, expect, actual):
 		""" 包含断言 """
 		# return AssertionFactory.create(method="contain").excute(expect=expect, actual=actual, name=name)
-		return cls.contain_strategy.excute(expect=expect, actual=actual, name=name)
+		return cls.contain_strategy.excute(expect=expect, actual=actual)
 
 	@classmethod
-	def uncontian(cls, expect, actual, name=None):
+	def uncontian(cls, expect, actual):
 		""" 不包含断言 """
 		# return AssertionFactory.create(method="uncontain").excute(expect=expect, actual=actual, name=name)
-		return cls.not_contain_strategy.excute(expect=expect, actual=actual, name=name)
-
+		return cls.not_contain_strategy.excute(expect=expect, actual=actual)
 
 # class AssertionFactory:
 # 	"""工厂类，根据传入的 method 创建相应的对象"""
