3fd3f140909ba2cec66461c95a1871d55ea3bae5
py85329140 <3293406747@qq.com>
2023-04-11 13:17:43 +0800

    优化代码结构

--- utils/generate_mock_data/sucreditcode.py
+++ utils/generate_mock_data/sucreditcode.py
@@ -84,7 +84,7 @@ ORGANIZATION_CHECK_CODE_DICT = {
 class CreditIdentifier(object):
     @staticmethod
     def get_random_address():
-        with open(Path(Path(__file__).parent,"data","address.json"),encoding="utf-8") as reader:
+        with open(Path(Path(__file__).parent, "data", "address.json"), encoding="utf-8") as reader:
             address = json.load(reader)
         nums_province = len(address)
         province = address[random.randint(0, nums_province - 1)]
